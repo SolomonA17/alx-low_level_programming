5ee2fba69e5135603d64da4a518b370437bd6ad6
Solomon Aberra <solomonaberra75@gmail.com>
2023-12-11 05:04:57 -0800

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/SolomonA17/alx-low_level_programming/master/0x18-dynamic_libraries/librand.so -O ../librand.so
-export LD_PRELOAD="../librand.so"
+wget -O /tmp/win.so https://github.com/SolomonA17/alx-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
