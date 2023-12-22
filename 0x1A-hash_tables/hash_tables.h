274a3a51f09fba90d2b40c35a746e8e908efaca0
Solomon Aberra <solomonaberra75@gmail.com>
2023-12-22 09:55:46 -0800

    0x1A. C - Hash tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -12,6 +13,7 @@
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
+
 typedef struct hash_node_s
 {
 	 char *key;
@@ -27,6 +29,7 @@ typedef struct hash_node_s
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
+
 typedef struct hash_table_s
 {
 	 unsigned long int size;
@@ -34,13 +37,13 @@ typedef struct hash_table_s
 } hash_table_t;
 
 /*functions*/
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_delete(hash_table_t *ht);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+unsigned long int hash_djb2(const unsigned char *str);
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 /**
  * struct shash_node_s - Node of a sorted hash table
@@ -52,6 +55,8 @@ void hash_table_delete(hash_table_t *ht);
  * @sprev: A pointer to the previous element of the sorted linked list
  * @snext: A pointer to the next element of the sorted linked list
  */
+
+
 typedef struct shash_node_s
 {
 	char *key;
@@ -71,6 +76,8 @@ typedef struct shash_node_s
  * @shead: A pointer to the first element of the sorted linked list
  * @stail: A pointer to the last element of the sorted linked list
  */
+
+
 typedef struct shash_table_s
 {
 	unsigned long int size;
@@ -79,11 +86,11 @@ typedef struct shash_table_s
 	shash_node_t *stail;
 } shash_table_t;
 
-shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-void shash_table_print(const shash_table_t *ht);
-char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
+void shash_table_print(const shash_table_t *ht);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 #endif /*_HASH_TABLES_H_*/
 
