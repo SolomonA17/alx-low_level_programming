daf0bba87fac9243c289cf2075702db7c7d380e9
Solomon Aberra <solomonaberra75@gmail.com>
2023-12-22 07:59:49 -0800

    0x1A. C - Hash tables

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,8 +4,8 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes a hash table.
- * @ht: A pointer to a hash table.
+ * hash_table_delete - free the hash table.
+ * @ht: hash_table_t pointer
  * Return: void.
  */
 void hash_table_delete(hash_table_t *ht)
