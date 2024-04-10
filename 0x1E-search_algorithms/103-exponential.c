c4bd35e3f3665bd3d6e4ac067fc825f2e97c8d7d
Solomon Aberra <solomonaberra75@gmail.com>
2024-04-10 02:09:06 -0700

    0x1E. C - Search Algorithms

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,18 +1,18 @@
 #include "search_algos.h"
 
 /**
- * _binary_search - Performs a binary search for a specified value in a sorted array
- *                  of integers.
- * @array: A pointer to the first element of the array to search.
- * @left: The starting index of the [sub]array to search.
- * @right: The ending index of the [sub]array to search.
- * @value: The value to search for.
- *
- * Return: If the value is not present or the array is NULL, -1.
- *         Otherwise, returns the index where the value is located.
- *
- * Description: Prints the [sub]array being searched after each change.
- */
+  * _binary_search - Searches for a value in a sorted array
+  *                  of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @left: The starting index of the [sub]array to search.
+  * @right: The ending index of the [sub]array to search.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         else, the index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
+  */
 int _binary_search(int *array, size_t left, size_t right, int value)
 {
 	size_t i;
@@ -40,17 +40,17 @@ int _binary_search(int *array, size_t left, size_t right, int value)
 }
 
 /**
- * exponential_search - Searches for a value in a sorted array
- *                      of integers using exponential search.
- * @array: A pointer to the first element of the array to search.
- * @size: The number of elements in the array.
- * @value: The value to search for.
- *
- * Return: If the value is not present or the array is NULL, -1.
- *         Otherwise, returns the index where the value is located.
- *
- * Description: Prints a value every time it is compared in the array.
- */
+  * exponential_search - Searches for a value in a sorted array
+  *                      of integers using exponential search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints a value every time it is compared in the array.
+  */
 int exponential_search(int *array, size_t size, int value)
 {
 	size_t i = 0, right;
@@ -61,11 +61,10 @@ int exponential_search(int *array, size_t size, int value)
 	if (array[0] != value)
 	{
 		for (i = 1; i < size && array[i] <= value; i = i * 2)
-			printf("Checked array[%ld] = [%d]\n", i, array[i]);
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	}
 
 	right = i < size ? i : size - 1;
 	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
 	return (_binary_search(array, i / 2, right, value));
 }
-
--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,13 +1,13 @@
 #include "search_algos.h"
 
 /**
- * linear_skip - Searches for a specified value in a sorted singly linked list
- *               of integers using linear skip.
- * @list: A pointer to the head of the linked list to search.
+ * linear_skip - Searches for an algorithm in a sorted singly
+ *               linked list of integers using linear skip.
+ * @list: A pointer to the  head of the linked list to search.
  * @value: The value to search for.
  *
  * Return: If the value is not present or the head of the list is NULL, NULL.
- *         Otherwise, a pointer to the first node where the value is located.
+ *         else, a pointer to the first node where the value is located.
  *
  * Description: Prints a value every time it is compared in the list.
  *              Uses the square root of the list size as the jump step.
@@ -25,7 +25,7 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		if (jump->express != NULL)
 		{
 			jump = jump->express;
-			printf("Checked value at index [%ld] = [%d]\n",
+			printf("Value checked at index [%ld] = [%d]\n",
 					jump->index, jump->n);
 		}
 		else
@@ -39,9 +39,8 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 			node->index, jump->index);
 
 	for (; node->index < jump->index && node->n < value; node = node->next)
-		printf("Checked value at index [%ld] = [%d]\n", node->index, node->n);
-	printf("Checked value at index [%ld] = [%d]\n", node->index, node->n);
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
 	return (node->n == value ? node : NULL);
 }
-
